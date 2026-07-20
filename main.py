660b3add6cf97c246df8ac50f9965b5315674f90
KebbaPhilip <kebbaphilip@gmail.com>
2026-07-20 16:53:33 +0000

    implement asteroind class and create asteroid group

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import pygame
+import asteroid
 from player import Player
+from asteroid import Asteroid
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+
     clock = pygame.time.Clock()
     dt = 0.0
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         log_state()
@@ -22,9 +31,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         #limits frame rate to 60FPS
