5300e0552098443ea585ed6bdde4187244cdeb09
KebbaPhilip <kebbaphilip@gmail.com>
2026-07-20 15:01:21 +0000

    setup player

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 
 def main():
     pygame.init()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     clock = pygame.time.Clock()
     dt = 0.0
 
@@ -19,7 +23,9 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
 
         #limits frame rate to 60FPS
         dt = clock.tick(60) / 1000
