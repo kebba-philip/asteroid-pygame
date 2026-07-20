2a565e3d9220c3ce441d840138eee4c5eedb54d9
KebbaPhilip <kebbaphilip@gmail.com>
2026-07-20 13:16:15 +0000

    set game frame rate

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         log_state()
+
+        #handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen.fill("black")
-            pygame.display.flip()
 
+        screen.fill("black")
+        pygame.display.flip()
 
-
-    print(f"Starting Asteroids with pygame version {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+        #limits frame rate to 60FPS
+        dt = clock.tick(60) / 1000
 
 
 
