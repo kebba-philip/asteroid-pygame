bcd675dbd6fdf15ea216c2c9a4cd696ef003b3be
KebbaPhilip <kebbaphilip@gmail.com>
2026-07-20 12:40:56 +0000

    setup event loop

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            screen.fill("black")
+            pygame.display.flip()
+
 
 
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
