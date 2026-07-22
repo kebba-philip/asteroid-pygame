01c7446b1374e4a78b1285d13f4fc81a3f74877e
KebbaPhilip <kebbaphilip@gmail.com>
2026-07-22 14:38:38 +0000

    create asterods

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 import asteroid
+import asteroidfield
 from player import Player
 from asteroid import Asteroid
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -14,8 +16,12 @@ def main():
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
+
     Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
